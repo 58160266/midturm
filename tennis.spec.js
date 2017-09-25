8fe545e159c6cfa8f3c64269f2568f3a01ad5f56
58160266 <58160266@go.buu.ac.th>
2017-09-25 22:42:40 +0700

    Created TennisGame()

--- tennis.spec.js
+++ tennis.spec.js
@@ -1,12 +1,30 @@
+function TennisGame() {
+  const scoreSrting = ['Love' , 'Fifteen' , 'Thirty','Forthy']
+  this.playerAScore = 0
+
+  this.reset = () =>{
+    this.playerAScore = 0
+  }
+
+  this.echo = () => {
+    return `${scoreSrting[this.playerAScore]} - Love`
+  }
+
+  this.playerAGetScore = () => {
+    this.playerAScore++
+  }
+}
+
 test('Echo "Love - Love" when game start',()=>{
   //Arrange
   let app = new TennisGame()
 
   //Act
   app.reset()
+  let result = app.echo()
 
   // Assert
-  expect(result).toBe(Love - Love);
+  expect(result).toBe('Love - Love');
 
 
 
